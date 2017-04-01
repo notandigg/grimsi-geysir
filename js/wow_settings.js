4bbd5c18748d8441e191bd296e44694997dfd3b2
Grimur <grimur@grimur.net>
2017-04-01 21:56:00 +0000

    Geysir version 1.3.0
    Features:
    — koken:search ready
    — Pjax and Nprogress for faster/smoother loading (content/essay)
    — Settings in admin, can change most to your liking
    — And much more
    
    — Please let me know if you find any bugs or if you have some ideas
    what could be better.

---
+++ js/wow_settings.js
@@ -0,0 +1,10 @@
+wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       false,       // default
+                      live:         true        // default
+                    }
+                    )
+                    wow.init();
